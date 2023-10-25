03fd3a7fa3e21906796839495e6c9ae826f65361
Carlos Belchior <carlos.belchior@selfridges.co.uk>
2023-10-25 09:53:26 +0100

    inclusion of wip script on package.json and a cleanup workflow on github

--- playwright-test/functional-tests/ui_tests/productListPage.spec.ts
+++ playwright-test/functional-tests/ui_tests/productListPage.spec.ts
@@ -6,7 +6,7 @@ import { test } from '../../helpers/fixtures'
 /*** Test Cases ***/
 
 test.describe('Product List Page', () => {
-  test('The PLP has all the requided sections', async ({ page, productListPage }) => {
+  test('@wip The PLP has all the requided sections', async ({ page, productListPage }) => {
     await productListPage.clickItemAtIndex(1)
 
     await page.pause()
