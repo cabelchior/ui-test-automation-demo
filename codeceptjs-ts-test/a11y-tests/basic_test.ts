b920bbe47c642f29dc099d0a3718ca085ba42cb7
Carlos Belchior <carlos.belchior@selfridges.co.uk>
2023-11-02 09:30:45 +0000

    Include a11y test with codeceptjs

--- codeceptjs-ts-test/a11y-tests/basic_test.ts
+++ codeceptjs-ts-test/a11y-tests/basic_test.ts
@@ -1,6 +1,10 @@
 Feature('Accessibility test on multiple pages'); //, { retryBefore: 2 });
 
 /*** Feature Global Variables ***/
+const credentials: Credentials = {
+  username: 'standard_user',
+  password: 'secret_sauce',
+};
 
 /*** Test Cases ***/
 
@@ -8,25 +12,34 @@ Before(({ I }) => {
   // do something...
 });
 
-Scenario('@wip Perform a11y check against the Login page @a11y', async ({ I, loginPage }) => {
+Scenario('Perform a11y check against the Login page @a11y', async ({ I, loginPage }) => {
   loginPage.goTo();
-  await I.runA11yCheck({ 
-    outputDir: 'output/loginPage', 
+
+  await I.runA11yCheck({
+    outputDir: 'output/loginPage',
   });
 
   // pause();
 });
 
-Scenario('@wip Perform a11y check against the Product List page @a11y', async ({ I, loginPage }) => {
-  let credentials: Credentials = {
-    username: 'standard_user',
-    password: 'secret_sauce',
-  };
-
+Scenario('Perform a11y check against the Product List page @a11y', async ({ I, loginPage }) => {
   loginPage.goTo();
   loginPage.logIn(credentials);
-  await I.runA11yCheck({ 
-    outputDir: 'output/productListPage', 
+  
+  await I.runA11yCheck({
+    outputDir: 'output/productListPage',
+  });
+
+  // pause();
+});
+
+Scenario('Perform a11y check against the Product Description page @a11y', async ({ I, loginPage, productListPage }) => {
+  loginPage.goTo();
+  loginPage.logIn(credentials);
+  productListPage.clickItemAtIndex(1);
+
+  await I.runA11yCheck({
+    outputDir: 'output/productDescriptionPage',
   });
 
   // pause();
